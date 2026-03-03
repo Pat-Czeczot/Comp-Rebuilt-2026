22c8a05980bab118949e566816e6d814c67a4712
Pat-Czeczot <patrick.czeczot08@gmail.com>
2026-03-03 16:40:03 -0500

    made fixes to pid

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,33 +10,43 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
-    
+
   SparkFlex shooter1;
   SparkFlex shooter2;
   private PIDController pid;
   private RelativeEncoder encoder;
+  private double targetSpeed = 0.0; // store the target so periodic can run the pid
 
   public Shooter() {
     shooter1 = new SparkFlex(Constants.Shooter1ID, MotorType.kBrushless);
     shooter1.setInverted(true);
     shooter2 = new SparkFlex(Constants.Shooter2ID, MotorType.kBrushless);
     shooter2.setInverted(true);
-    encoder = shooter1.getExternalEncoder();
-    encoder = shooter2.getExternalEncoder();
+
+    encoder = shooter1.getExternalEncoder(); // only assign once using shooter1s encoder
 
     pid = new PIDController(0.01, 0, 0);
-
   }
-    public void setSpeed(double speed){
-      shooter1.set(speed);
-      shooter2.set(-speed);
 
-      setSpeed(MathUtil.clamp(pid.calculate(encoder.getPosition(), speed), 0.8, 0.9));
-
+  // setspeed now just stores the target
+  public void setSpeed(double speed) {
+    targetSpeed = speed;
   }
+
   @Override
   public void periodic() {
+    double currentRPM = encoder.getVelocity();
+
+    // pid calculates correction based on actual RPM vs the target RPM
+    double pidOutput = pid.calculate(currentRPM, targetSpeed);
+
+    // clamp the output to a safe range
+    double clampedOutput = MathUtil.clamp(pidOutput, -1.0, 1.0);
+
+    shooter1.set(clampedOutput);
+    shooter2.set(-clampedOutput);
   }
+
   @Override
   public void simulationPeriodic() {
   }
